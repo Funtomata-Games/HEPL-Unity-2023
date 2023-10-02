26ffe33d26d38b7f654df8b763d9988f1641c871
jeango <jdc@funtomata.com>
2023-10-02 15:32:31 +0200

    fin de cours

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MovementController : MonoBehaviour
+{
+    public Vector2 direction;
+    public int speed;
+
+    public void OnDirectionChange(InputAction.CallbackContext context)
+    {
+        direction = context.ReadValue<Vector2>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime * speed, Space.World);
+    }
+}
