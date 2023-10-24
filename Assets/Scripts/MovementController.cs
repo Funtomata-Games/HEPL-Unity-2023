6b4b816dc12ea8c3802c809beddc988b2eec7497
jeango <jdc@funtomata.com>
2023-10-24 10:37:52 +0200

    Fin cours 4

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,6 +6,7 @@ public class MovementController : MonoBehaviour
 {
     public Vector2 direction;
     public int speed;
+    public Rigidbody2D body;
 
     // Update is called once per frame
     void Update()
@@ -14,13 +14,25 @@ public class MovementController : MonoBehaviour
         Move();
     }
 
+    private void FixedUpdate()
+    {
+        PhysicsMove();
+    }
+
+
     public void OnDirectionChange(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
     }
-    
+    private void PhysicsMove()
+    {
+        if (body && !body.isKinematic)
+            body.velocity = direction * speed;
+    }
+   
     private void Move()
     {
-        transform.Translate(direction.normalized * (Time.deltaTime * speed), Space.World);
+        if (!body || body.isKinematic)
+            transform.Translate(direction.normalized * (Time.deltaTime * speed), Space.World);
     }
 }
