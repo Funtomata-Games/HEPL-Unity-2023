ddd7560c603f5a574814a76c3b5d66ffd27c23b1
jeango <jdc@funtomata.com>
2023-11-14 12:41:42 +0100

    Fin cours 6

--- Assets/Scripts/Damage.cs
+++ Assets/Scripts/Damage.cs
@@ -1,14 +1,25 @@
-using System;
 using System.Linq;
 using UnityEngine;
 
 public class Damage : MonoBehaviour
 {
    public string[] damageableTags;
+   public bool killTarget;
 
    private void OnTriggerEnter2D(Collider2D col)
    {
       if (damageableTags.Contains(col.tag))
-         Destroy(col.gameObject);
+         KillOrDestroy(col.gameObject);
+   }
+
+   private void KillOrDestroy(GameObject target)
+   {
+      if (!killTarget) Destroy(target);
+      else
+      {
+         var deathCtrl = target.GetComponentInChildren<DeathController>();
+         if (!deathCtrl) Destroy(target);
+         else deathCtrl.Kill();
+      }
    }
 }
